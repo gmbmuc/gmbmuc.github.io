d0f238a403df6c921aeb701872dd4e1612de2dcd
gmbmuc <info@gruener-migrationsbeirat-muenchen.de>
2023-12-13 16:49:45 +0100

    moved social to top

--- src/views/partials/index.ts
+++ src/views/partials/index.ts
@@ -2,5 +2,6 @@ import footer from 'bundle-text:./footer.mustache';
 import header from 'bundle-text:./header.mustache';
 import main from 'bundle-text:./main.mustache';
 import members from 'bundle-text:./members.mustache';
+import social from 'bundle-text:./social.mustache';
 
-export { footer, header, main, members };
+export { footer, header, main, members, social };
